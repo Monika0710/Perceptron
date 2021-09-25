5d0ebdeb25528ecd531761bacccc37e81b404a5c
monika bhargav <monikabhargav11@gmail.com>
2021-09-25 22:55:36 +0530

    organized code

--- and.py
+++ and.py
@@ -4,26 +4,34 @@ import pandas as pd
 import numpy as np
 from utils.all_utils import save_model,save_plot
 
+def main(data,eta,epoch,model_File,Plot_File):
 
-AND = {
-    "x1": [0,0,1,1],
-    "x2": [0,1,0,1],
-    "y": [0,0,0,1],
-}
+    
+    df = pd.DataFrame(data)
 
-df = pd.DataFrame(AND)
-
-X,y=prepare_data(df)
+    X,y=prepare_data(df)
 
 
-ETA = 0.3 # 0 and 1
-EPOCHS = 10
+    
+    model= Perceptron(eta=eta, epoch=epoch)
+    model.fit(X, y)
 
-model_AND = Perceptron(eta=ETA, epoch=EPOCHS)
-model_AND.fit(X, y)
+    # _ = model_AND.total_loss()
 
-# _ = model_AND.total_loss()
+    save_model(model,filename=model_File)
 
-save_model(model_AND,filename="and.model")
+    save_plot(df,Plot_File,model)
 
-save_plot(df,"and.png",model_AND)
+
+if __name__ =='__main__':
+    AND = {
+        "x1": [0,0,1,1],
+        "x2": [0,1,0,1],
+        "y": [0,0,0,1],
+    }
+    ETA = 0.3 # 0 and 1
+    EPOCHS = 10
+
+
+    main(AND,ETA,EPOCHS,"and.model","and.png")
+
--- or.py
+++ or.py
@@ -4,26 +4,34 @@ import pandas as pd
 import numpy as np
 from utils.all_utils import save_model,save_plot
 
+def main(data,eta,epoch,model_File,Plot_File):
 
-OR = {
-    "x1": [0,0,1,1],
-    "x2": [0,1,0,1],
-    "y": [0,1,1,1],
-}
+    
+    df = pd.DataFrame(data)
 
-df = pd.DataFrame(OR)
-
-X,y=prepare_data(df)
+    X,y=prepare_data(df)
 
 
-ETA = 0.3 # 0 OR 1
-EPOCHS = 10
+    
+    model= Perceptron(eta=eta, epoch=epoch)
+    model.fit(X, y)
 
-model_OR = Perceptron(eta=ETA, epoch=EPOCHS)
-model_OR.fit(X, y)
+    # _ = model_AND.total_loss()
 
-# _ = model_OR.total_loss()
+    save_model(model,filename=model_File)
 
-save_model(model_OR,filename="OR.model")
+    save_plot(df,Plot_File,model)
 
-save_plot(df,"OR.png",model_OR)
+
+if __name__ =='__main__':
+    OR = {
+        "x1": [0,0,1,1],
+        "x2": [0,1,0,1],
+        "y": [0,1,1,1],
+    }
+    ETA = 0.3 # 0 and 1
+    EPOCHS = 10
+
+
+    main(OR,ETA,EPOCHS,"or.model","or.png")
+
